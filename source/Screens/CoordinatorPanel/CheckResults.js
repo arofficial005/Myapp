b98d587532d539db5c55b5c7aa921b919ba0b3fe
arofficial005 <abdurrehman4009@gmail.com>
2022-08-10 15:21:16 +0500

    added delete functionality

--- source/Screens/CoordinatorPanel/CheckResults.js
+++ source/Screens/CoordinatorPanel/CheckResults.js
@@ -1,39 +1,74 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React,{useState,useEffect} from 'react'
+import { StyleSheet, Text, View ,TouchableOpacity,Alert} from 'react-native'
+import React,{useState,useEffect,useCallback} from 'react'
 import firestore from '@react-native-firebase/firestore';
-
+import { useNavigation } from '@react-navigation/native';
 const CheckResults = (props) => {
+  const navigation=useNavigation()
   const email=props.route.params.email
 const [results, setResults] = useState([])
+const [ids, setIds] = useState([])
 useEffect(() => {
-  firestore()
-  .collection('Voting')
-  // Filter results
-  .where('email', '==', email)
-  // Limit results
-  .get()
-  .then(querySnapshot => {
-    /* ... */
-    console.log(querySnapshot.size)
-    querySnapshot.forEach(documentSnapshot => {
-      // console.log('User ID: ', documentSnapshot.id, documentSnapshot.data());
-      setResults((results)=>[...results,documentSnapshot.data()])
-    });
-  });
-
- 
+  FetchData() 
 }, [])
+const DeleteData=useCallback(
+  (id) => {
+    firestore()
+  .collection('Voting')
+  .doc(id)
+  .delete()
+  .then(() => (
+    Alert.alert(
+      "Alert Title",
+      "My Alert Msg",
+      [
+       
+        { text: "OK", onPress: ()=>navigation.goBack() }
+      ]
+    )
+  ));
+  },
+  [],
+)
+const FetchData=useCallback(
+  () => {
+    console.log("FEtch")
+    firestore()
+    .collection('Voting')
+    // Filter results
+    .where('email', '==', email)
+    // Limit results
+    .get()
+    .then(querySnapshot => {
+      /* ... */
+      // console.log(querySnapshot.size)
+      querySnapshot.forEach(documentSnapshot => {
+        // console.log('User ID: ', documentSnapshot.id, documentSnapshot.data());
+        setIds(ids=>[...ids,documentSnapshot.id])
+        setResults(results=>[...results,documentSnapshot.data()])
+      });
+    });
+  
+  },
+  [setIds,setResults,ids,results,email],
+)
 
   return (
     <View>
       <Text>CheckResults</Text>
       {results?.map((item,index)=>{
-        console.log(item.data)
+        // console.log(ids)
         return(
           <>
+          <TouchableOpacity onPress={()=>DeleteData(ids[index])}>
+            <Text>Delete</Text>
+          </TouchableOpacity>
+          <Text>{item.role}</Text>
           {item.data.map((object,index)=>(
-          <Text>name:</Text>
-
+            <>
+          <Text>Name: {object?.name}</Text>
+          <Text>Roll #: {object?.rollno}</Text>
+          <Text>Department: {object?.dept}</Text>
+            </>
       ))}
           </>
 
